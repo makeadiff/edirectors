6b7d27186c7dbec57d5b059f516a556fdb752420
Binny V A <binnyva@gmail.com>
2017-08-31 18:03:12 +0530

    Only the current directors will be shown. Old directors will be moved to a different section

--- common.php
+++ common.php
@@ -3,4 +3,4 @@ require '../common.php';
 
 extract(check_user());
 
-if($user_id != 1) die("This section is only accessable by Binny. Ask him for permission to access this section");
+if($user_id != 1 and $user_id != 57184) die("This section is only accessable by Binny and Rohit. Ask him for permission to access this section");
--- old.php
+++ old.php
@@ -8,9 +8,9 @@ $directors->setTitle("Director", "Directors");
 $directors->addField("image", "Image", 'text');
 $directors->addField("user_id", "", 'int', array(), '', 'hidden');
 
-$directors->setListingQuery("SELECT * FROM App_Director WHERE status='1'");
+$directors->setListingQuery("SELECT * FROM App_Director WHERE status='0'");
 
 $directors->setListingFields("name","role",'email','image','sort','status');
-$directors->code['bottom'] = '<a href="old.php">Old Directors</a>';
+$directors->code['bottom'] = '<a href="index.php">Current Directors</a>';
 $directors->render();
 //render();
